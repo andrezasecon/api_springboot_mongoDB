b4dc91087626483df3141b945fce187adb8a631d
Andreza Secon <andrezasecon@gmail.com>
2021-04-23 18:15:11 -0300

    Created project

---
+++ src/main/java/com/andreza/apirestmongo/ApirestmongoApplication.java
@@ -0,0 +1,13 @@
+package com.andreza.apirestmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreza/apirestmongo/ApirestmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreza.apirestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
