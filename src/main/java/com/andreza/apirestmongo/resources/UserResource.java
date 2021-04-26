292043e7ce2f570c3d3d278884e9d05a70092a75
Andreza Secon <andrezasecon@gmail.com>
2021-04-26 17:25:11 -0300

    Implementado insert de User

--- src/main/java/com/andreza/apirestmongo/resources/UserResource.java
+++ src/main/java/com/andreza/apirestmongo/resources/UserResource.java
@@ -5,10 +5,10 @@ import com.andreza.apirestmongo.dto.UserDTO;
 import com.andreza.apirestmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,5 +29,12 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+    @RequestMapping(method= RequestMethod.POST)
+    public ResponseEntity<Void> insertUser (@RequestBody UserDTO objDTO){
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{/id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/andreza/apirestmongo/services/UserService.java
+++ src/main/java/com/andreza/apirestmongo/services/UserService.java
@@ -1,9 +1,11 @@
 package com.andreza.apirestmongo.services;
 
 import com.andreza.apirestmongo.domain.User;
+import com.andreza.apirestmongo.dto.UserDTO;
 import com.andreza.apirestmongo.repository.UserRepository;
 import com.andreza.apirestmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
