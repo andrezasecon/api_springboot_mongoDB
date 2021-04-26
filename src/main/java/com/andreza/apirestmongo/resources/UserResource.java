f2e3c74d02e25257914e754f1ec52fde832a6b8b
Andreza Secon <andrezasecon@gmail.com>
2021-04-26 15:07:33 -0300

    Criando DTO para User

--- src/main/java/com/andreza/apirestmongo/resources/UserResource.java
+++ src/main/java/com/andreza/apirestmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.andreza.apirestmongo.resources;
 
 import com.andreza.apirestmongo.domain.User;
+import com.andreza.apirestmongo.dto.UserDTO;
 import com.andreza.apirestmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
