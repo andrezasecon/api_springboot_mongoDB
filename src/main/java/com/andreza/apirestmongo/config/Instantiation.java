76a0fa9fa06808a87ca05f6cbadc09c091f9c365
Andreza Secon <andrezasecon@gmail.com>
2021-04-26 14:25:10 -0300

    Instanciação da base de dados com @configiguration

---
+++ src/main/java/com/andreza/apirestmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.andreza.apirestmongo.config;
+
+import com.andreza.apirestmongo.domain.User;
+import com.andreza.apirestmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner  {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex= new User (null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+
+    }
+}
